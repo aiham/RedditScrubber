7dcb617f8310a32aadba953c1a174a9b164b3f13
Aiham Hammami <aiham@aiham.net>
2014-08-05 14:39:59 +1000

    Add status messages to deploy script

--- web/scripts/deploy.sh
+++ web/scripts/deploy.sh
@@ -8,7 +8,12 @@ config_dir="${project_dir}/config"
 git pull
 npm install
 bower install
+echo "Migrating db..."
 npm run-script migrate
+echo "Copying config..."
 cp ~/config/*.json "${config_dir}/"
+echo "Restarting reddit-scrubber..."
 sudo monit restart reddit-scrubber
+echo "Restarting reddit-scrubber-bot..."
 sudo monit restart reddit-scrubber-bot
+echo "Done."
