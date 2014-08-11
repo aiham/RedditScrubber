2dbb26fd05537a8948044818ee90d17208e3bd1a
Aiham Hammami <aiham@aiham.net>
2014-08-11 16:49:06 +1000

    Simplify default grunt command
    So it only builds files, does not watch

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -50,7 +50,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('build', ['sass', 'copy']);
-  grunt.registerTask('default', ['build', 'watch']);
+  grunt.registerTask('default', ['sass', 'copy']);
 
 };
